f96322bd8fc9f22831ae5b526452a59d6346b650
Chancellor Holtzman <me@chancellor.tech>
2023-02-22 09:15:28 +0000

    :sparkles: working eslint, prettier, TS

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,6 +5,6 @@ module.exports = {
 	trailingComma: 'es5',
 	tabWidth: 2,
 	printWidth: 120,
-	pluginSearchDirs: ['.'],
+	plugins: ['prettier-plugin-svelte'],
 	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
 };
